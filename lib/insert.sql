560e4137bd45c8d07295d70472d20f2c1a62989d
Mark <markkamamia@gmail.com>
2024-06-05 16:43:22 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",21,"M","black","high",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy", 13 , "M", "gold", "warm", FALSE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha", 11, "F", "white", "optimistic", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 12 , "M", "brown", "relaxed", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa", 10 , "F", "white", "slow-moving", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch", 10 , "M", "brown", "nostalgic", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy", 10 , "F", "gray", "analytical", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL, 9 , "M" , "black" , "high" , TRUE);
